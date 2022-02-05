185eeff102694da63f47869b64b9b918c8ce8401
TomIshiyama <honngannji@gmail.com>
2022-02-05 12:26:04 +0900

    inital commit next.js enviroment

--- client/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Admin from "./Admin"
-import Events from "./Events"
-import Home from "./Home"
-import Login from "./Login"
-import Setting from "./Setting"
-import Signup from "./Signup"
-import User from "./User"
-
-export { Admin, Home, Login, Signup, Setting, User, Events }
