53405ac2abb0847b38d4352a949d91d5f00440d3
TomIshiyama <honngannji@gmail.com>
2022-03-18 21:24:24 +0900

    feat: Storyの設定を追加

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,11 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
-}
+    stories: [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)",
+        {
+            // 👇 The directory field sets the directory your stories
+            // directory: "../packages/stories",
+            // 👇 The titlePrefix field will generate automatic titles for your stories
+            titlePrefix: "MyComponents",
+            // 👇 Storybook will load all files that contain the stories extensio
+            // files: "*.stories.*",
+        },
+    ],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    framework: "@storybook/react",
+};
