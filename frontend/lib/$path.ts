9d9baf3980cd95fd2044a3b93f2056a2cec00768
TomIshiyama <honngannji@gmail.com>
2022-03-21 23:30:44 +0900

    🐞fix: 不要なファイルを削除

--- frontend/lib/$path.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-// prettier-ignore
-export const pagesPath = {
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
-
-// prettier-ignore
-export type PagesPath = typeof pagesPath
