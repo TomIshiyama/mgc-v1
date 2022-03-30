8a4522d3e0171bff3c0d0e2d9102f54a5f166175
TomIshiyama <honngannji@gmail.com>
2022-03-31 02:43:08 +0900

    ✨feat: CSSスタイル用の定数ファイルを新規作成

---
+++ frontend/src/utils/styling.ts
@@ -0,0 +1,12 @@
+// FIXME:要調整
+export const COLOR = {
+    meeting: "aqua",
+    tech: "gold",
+    meetup: "hotPink", // deepPink
+
+    dark: {},
+    normal: {
+        bgcolor: "white", //
+        sideBgcolor: "SeaShell", // 他候補 OldLace,Line
+    },
+} as const;
