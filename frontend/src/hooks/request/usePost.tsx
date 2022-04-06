af4a54921e2976cd6c8a9a42247fc022c68da598
TomIshiyama <honngannji@gmail.com>
2022-04-06 22:16:32 +0900

    ✨feat: usePostカスタムフック新規作成

---
+++ frontend/src/hooks/request/usePost.tsx
@@ -0,0 +1,79 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse, Method } from "axios";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+interface Params {
+    [key: string]: any;
+}
+
+interface Args<T> {
+    url?: string;
+    params?: Params;
+    body?: Params;
+    method?: Method;
+    onSuccess?: (data?: T) => void;
+    onError?: (err: AxiosError) => void;
+}
+
+interface PostRequest {
+    method?: Method;
+    url?: string;
+    params?: Params;
+    body?: Params;
+    headers?: AxiosRequestConfig["headers"];
+}
+
+type DoPost<T> = ({ url, params }: Args<T>) => Promise<void>;
+
+interface PostResponse<T> {
+    doPost: DoPost<T>;
+    isLoading: boolean;
+}
+
+export function usePost<T>({
+    method = "post",
+    url,
+    params,
+    headers,
+}: PostRequest): PostResponse<T> {
+    const methodInternal = useMemo(() => method, [method]);
+    const urlInternal = useMemo(() => url, [url]);
+    const paramsInternal = useMemo(() => params, [params]);
+    const [isLoading, setLoading] = useState(false);
+    const doPost = useCallback<DoPost<T>>(
+        async <T,>(args: Args<T>) => {
+            const reqUrl = args.url ?? urlInternal ?? "";
+            if (!reqUrl) {
+                return;
+            }
+            if (!methodInternal) {
+                return;
+            }
+            try {
+                setLoading(true);
+                const res: AxiosResponse<T> = await axios.request({
+                    method: args?.method ?? methodInternal,
+                    url: reqUrl,
+                    data: args?.params ?? paramsInternal ?? undefined,
+                    headers,
+                });
+                args.onSuccess?.(res.data);
+            } catch (err) {
+                args.onError?.(err as AxiosError);
+            } finally {
+                setLoading(false);
+            }
+        },
+        [headers, methodInternal, paramsInternal, urlInternal]
+    );
+
+    const clear = useCallback(() => {
+        setLoading(false);
+    }, []);
+
+    useEffect(() => () => clear(), [clear]);
+
+    return {
+        doPost,
+        isLoading,
+    };
+}
