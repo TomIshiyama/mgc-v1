ee3bfa3e75bd0f4e0de8cf4a73aa16324a30ea5e
TomIshiyama <honngannji@gmail.com>
2022-06-10 22:16:47 +0900

    ✨feat: next-auth 導入 session providerでsessionを配布

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,7 +3,10 @@ import "modern-css-reset/dist/reset.min.css"; // CSSのリセット
 import moment from "moment";
 import "moment/locale/ja";
 import { NextPage } from "next";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import React, { ReactElement, ReactNode } from "react";
 import "react-big-calendar/lib/css/react-big-calendar.css"; // カレンダーのスタイルを取り込む
 import "react-date-range/dist/styles.css"; // react-date-range main css file
@@ -23,7 +26,11 @@ type AppPropsWithLayout = AppProps & {
 };
 
 // TS の場合はAppPropsを拡張しないとうまくLayoutの型エラーが解決できない
-function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+function MyApp({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppPropsWithLayout): JSX.Element {
+    const { push } = useRouter();
     // HACK: カスタムフックきりわけ
     // FIXME: ローダー実装
     const [loading, setLoading] = React.useState<boolean | undefined>(false);
@@ -41,6 +48,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
     //     router.events.on("routeChangeComplete", handleComplete);
     //     router.events.on("routeChangeError", handleComplete);
     // }, [router]);
+    // useRedirectAuth();
 
     // ページごとに定義されたレイアウトがある場合はそれを使用する
     const getLayout = Component.getLayout ?? ((page) => page);
@@ -49,15 +57,17 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
         <Typography variant="h1">Loading...（仮実装）</Typography>
     ) : (
         <>
-            <MaterialThemeProvider>
-                <MediaQueryProvider>
-                    <FetchEventProvider>
-                        <DetailDrawerProvider>
-                            {getLayout(<Component {...pageProps} />)}
-                        </DetailDrawerProvider>
-                    </FetchEventProvider>
-                </MediaQueryProvider>
-            </MaterialThemeProvider>
+            <SessionProvider session={session as Session}>
+                <MaterialThemeProvider>
+                    <MediaQueryProvider>
+                        <FetchEventProvider>
+                            <DetailDrawerProvider>
+                                {getLayout(<Component {...pageProps} />)}
+                            </DetailDrawerProvider>
+                        </FetchEventProvider>
+                    </MediaQueryProvider>
+                </MaterialThemeProvider>
+            </SessionProvider>
         </>
     );
 }
