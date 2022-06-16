9b18b2255896f00a0d2c69790cc5d58b4f1da781
TomIshiyama <honngannji@gmail.com>
2022-06-16 23:57:26 +0900

    ✨feat: バックエンド２起動スクリプト 必要なファイルと処理を作成

---
+++ backend2/db/init_db.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#wait for the MySQL Server to come up
+#sleep 90s
+
+#run the setup script to create the DB and the schema in the DB
+# TODO: ほんまはDockerコンテナから立ち上げたい
+mysql.server stop && mysql.server start
+
+CURRENT=$(cd $(dirname $0);pwd)
+
+FILE_PATH=`pwd -P`
+# echo $FILE_PATH
+echo "execute file :$CURRENT/init.db.sh"
+echo "sql file :$CURRENT/init_mgc_db.sql"
+mysql --defaults-extra-file=my.cnf -h localhost mgc < "$CURRENT/init_mgc_db.sql"
