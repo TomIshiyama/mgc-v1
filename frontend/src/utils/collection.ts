89efeda298124139198dc1b6d386d84d5235737d
TomIshiyama <honngannji@gmail.com>
2023-02-23 16:26:24 +0900

    🐞fix: type difinition.

--- frontend/src/utils/collection.ts
+++ frontend/src/utils/collection.ts
@@ -13,11 +13,12 @@ export const parseValuesDateString = <T = any>(value: T) =>
  * @param object
  * @returns プロパティのValueがDateの場合、timestampに変換
  */
-export const mapDateString = <T>(object: T): Serialized<T> =>
+export const mapDateString = <T extends string>(object: T): Serialized<T> =>
     Object.entries(object).reduce((accum, [key, val], idx) => {
         return { ...accum, [key]: parseValuesDateString(val) };
     }, {} as Serialized<T>);
 
+/** Nullishを省いた配列を返却する */
 export const excludeNullish = <T>(array: T[]): NonNullable<T>[] => {
     const data = array.filter((v) => v != null);
     return data as unknown as NonNullable<T>[];
